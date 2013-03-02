d8ac1dbacd0ed8c93a1c8becfcc1432d63734462
tytan <git@tytanos.pl>
2013-03-02 17:57:12 +0100

    1 wrzut
    wrzut pierwszy

---
+++ RFM12B_simple_receive.ino
@@ -0,0 +1,32 @@
+#include <JeeLib.h>
+
+typedef struct {
+	int lp;
+	int bat;
+} Payload;
+Payload pomiar;
+
+void setup () {
+    Serial.begin(57600);
+    Serial.println("\n[Sensnode Demo]");
+    rf12_initialize('R', RF12_433MHZ, 210);
+     // rf12_control(0xC688); // bitrate =4.789kbps
+     rf12_control(0xC691); //bitrate 2.395kbps
+     rf12_control(0x9820); // deviation 45kHz
+     rf12_control(0x94C2); //BW 67kHz
+     rf12_control(0xC0C3); //batttery and uC clock 5MHz
+
+
+}
+
+void loop () {
+    if (rf12_recvDone() && rf12_crc == 0 && rf12_len == sizeof pomiar) {
+        memcpy(&pomiar, (byte*) rf12_data, sizeof pomiar);
+        Serial.print("WS ");
+        Serial.print("LP");
+        Serial.print(pomiar.lp);
+        Serial.print(" T");
+        Serial.print(pomiar.bat);
+        Serial.println();
+    }
+}
